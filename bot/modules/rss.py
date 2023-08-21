5c537fc398b1713cbfaf6c691dd55b62f9024e0b
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-08-22 00:49:11 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -617,7 +617,6 @@ async def rssMonitor():
                     else:
                         feed_msg = f"<b>Name: </b><code>{item_title.replace('>', '').replace('<', '')}</code>\n\n"
                         feed_msg += f"<b>Link: </b><code>{url}</code>"
-                    feed_msg += f"\n<b>Tag: </b><code>{data['tag']}</code> <code>{user}</code>"
                     await sendRss(feed_msg)
                     feed_count += 1
                 async with rss_dict_lock:
